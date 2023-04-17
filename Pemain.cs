670fd35719098f2babb4b119d419a3c94bcc3ea9
nizarmamk <nizarmbtp@students.amikom.ac.id>
2023-04-17 23:41:14 +0700

    Praktikum6

---
+++ Pemain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramEnkapsulasi
+{
+    internal class Pemain
+    {
+        // Fields
+        public string nama;
+        public string klub;
+        public string kebangsaan;
+        public string liga;
+        public int umur;
+        public int nomor;
+
+        // Constructor
+        public Pemain(string nama, string klub, string kebangsaan, string liga, int umur, int nomor)
+        {
+            this.nama = nama;
+            this.klub = klub;
+            this.kebangsaan = kebangsaan;
+            this.liga = liga;
+            this.umur = umur;
+            this.nomor = nomor;
+        }
+
+        public Pemain()
+        {
+
+        }
+
+        // Methods
+        public void SetDataDiri(string nama, int umur, string kebangsaan)
+        {
+            this.nama = nama;
+            this.umur = umur;
+            this.kebangsaan = kebangsaan;
+            Console.WriteLine("Nama Pemain      : {0}", this.nama);
+            Console.WriteLine("Umur Pemain      : {0}", this.umur);
+            Console.WriteLine("Kebangsaan       : {0}", this.kebangsaan);
+        }
+
+        public void SetDataKlub(string liga, string klub, int nomor)
+        {
+            this.liga = liga;
+            this.klub = klub;
+            this.nomor = nomor;
+            Console.WriteLine("Liga Klub Pemain : {0}", this.liga);
+            Console.WriteLine("Klub Pemain      : {0}", this.klub);
+            Console.WriteLine("Nomor Punggung   : {0}", this.nomor);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramEnkapsulasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Program Data Pemain Sepak Bola Eropa ");
+            Console.WriteLine("======================================");
+            
+            Pemain Pemain1 = new Pemain();
+            Pemain Pemain2 = new Pemain();
+            Pemain Pemain3 = new Pemain();
+
+            Pemain1.SetDataDiri("Alejandro Garnacho", 18, "Argentina");
+            Pemain1.SetDataKlub("Premier League", "Manchester United", 49);
+            Console.WriteLine();
+
+            Pemain2.SetDataDiri("Karim Benzema", 35, "Perancis");
+            Pemain2.SetDataKlub("La Liga", "Real Madrid", 9);
+            Console.WriteLine();
+
+            Pemain3.SetDataDiri("Zlatan Ibrahimovic", 41, "Swedia");
+            Pemain3.SetDataKlub("Serie A", "AC Milan", 11);
+            Console.WriteLine();
+        }
+    }
+}
